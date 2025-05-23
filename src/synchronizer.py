72b33a877fb67d54f518a5dd17ff255868ab8da6
nikolamaksic <nikola7amg@gmail.com>
2025-05-24 00:29:35 +0200

    fix: issues with processing file throw the gui

--- src/synchronizer.py
+++ src/synchronizer.py
@@ -10,21 +10,40 @@ class Synchronizer:
 
     def __init__(self, subtitle_delay_start_t: tuple = (0,0,0), subtitle_delay: \
          tuple = (0,0,0), mode=1, input_file=None, output_file=None):
-        self.subtitle_delay = subtitle_delay
+        minute, second, microsecond = subtitle_delay
+        self.__subtitle_delay = timedelta(minutes=minute, seconds=second, milliseconds=microsecond)
         minute, second, microsecond = subtitle_delay_start_t
-        self.subtitle_delay_start_t = time(hour=0, minute=minute, second=second, microsecond=microsecond)
+        self.__subtitle_delay_start_t = time(hour=0, minute=minute, second=second, microsecond=microsecond)
         self.mode = mode
         self.input_file = input_file
-        self._output_file  = output_file or self.create_output_file()
+        self.__output_file  = output_file or self.create_output_file()
+
+    @property
+    def subtitle_delay(self):
+        return self.__subtitle_delay
+    
+    @subtitle_delay.setter
+    def subtitle_delay(self, value):
+        minute, second, microsecond = value
+        self.__subtitle_delay = timedelta(minutes=minute, seconds=second, milliseconds=microsecond)
+
+    @property
+    def subtitle_delay_start_t(self):
+        return self.__subtitle_delay_start_t
+    
+    @subtitle_delay_start_t.setter
+    def subtitle_delay_start_t(self, value):
+        minute, second, microsecond = value
+        self.__subtitle_delay_start_t = time(hour=0, minute=minute, second=second, microsecond=microsecond)
 
     @property
     def output_file(self):
-        return self._output_file 
+        return self.__output_file 
 
     @output_file.setter
     def output_file(self, value):
         print(value)
-        self._output_file  = value or self.create_output_file()
+        self.__output_file  = value or self.create_output_file()
 
     def create_output_file(self):
         if not self.input_file:
@@ -41,12 +60,10 @@ class Synchronizer:
         return f"{t_str.strftime('%H:%M:%S')},{int(t_str.microsecond / 1000):03d}"
 
     def get_delayed_line(self, start_t, end_t):
-        m, s, ms = self.subtitle_delay
-
         new_start_t = self.datetime_str_format(start_t + \
-            (self.mode)*timedelta(minutes=m, seconds=s, milliseconds=ms))
+            (self.mode)*self.__subtitle_delay)
         new_end_t = self.datetime_str_format(end_t + \
-            (self.mode)*timedelta(minutes=m, seconds=s, milliseconds=ms))
+            (self.mode)*self.__subtitle_delay)
         new_line = f"{new_start_t} --> {new_end_t}\n"
         return new_line
 
@@ -54,17 +71,17 @@ class Synchronizer:
         if start_t and end_t:
             start_time = self.get_datetime(start_t)
             end_time = self.get_datetime(end_t)
-            if start_time.time() > self.subtitle_delay_start_t:
+            if start_time.time() > self.__subtitle_delay_start_t:
                 new_line = self.get_delayed_line(start_time, end_time)
                 return new_line
         return line
 
     def process(self):
-        if not self._output_file:
-            self._output_file = self.create_output_file()
+        if not self.__output_file:
+            self.__output_file = self.create_output_file()
         
         file_parser = SubtitleFileParser(self.input_file)
-        with open(self._output_file, "w") as out_f:
+        with open(self.__output_file, "w") as out_f:
             with file_parser as parser:
                 for line, start_t, end_t in parser:
                     new_line = self.get_updated_line(start_t, end_t, line)
